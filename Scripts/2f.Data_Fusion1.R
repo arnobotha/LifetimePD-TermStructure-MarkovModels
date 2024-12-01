6606ebd1388f61d2bd1f2fbd71c149e59476890a
Arno Botha <arno.spasie.botha@gmail.com>
2024-12-01 07:34:46 +0200

    Housekeeping

--- Scripts/2f.Data_Fusion1.R
+++ Scripts/2f.Data_Fusion1.R
@@ -51,6 +51,10 @@ ptm <- proc.time() # for runtime calculations (ignore)
 if (!exists('datCredit_real')) unpack.ffdf(paste0(genPath,"creditdata_final3"), tempPath)
 if (!exists('datMV')) unpack.ffdf(paste0(genPath,"datMV"), tempPath)
 if (!exists('datExclusions')) unpack.ffdf(paste0(genObjPath,"Exclusions-TruEnd"), tempPath)
+if (!exists('maxDate_observed') | !exists('minDate_observed')){
+  (maxDate_observed <- max(datCredit_real$Date, na.rm=T) )
+  (minDate_observed <- rollback(min(datCredit_real$Date, na.rm=T), roll_to_first = T) )
+}
 
 # [DIAGNOSTIC] Account-level and dataset-wide impacts of exclusion
 # First, create an intermediary variable as an expediency
