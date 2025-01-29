7de9e8205da4425d2800fdf96a76125c9e5f118d
Arno Botha <arno.spasie.botha@gmail.com>
2025-01-29 11:21:54 +0200

    Update PseudoRSquared_BetaRegression.R

--- Scripts/Proof of Concepts/PseudoRSquared_BetaRegression.R
+++ Scripts/Proof of Concepts/PseudoRSquared_BetaRegression.R
@@ -12,12 +12,23 @@ summary(m)
 # --- Calculate pseudo R^2 from first principles
 # Ferrari2004 describe their pseudo R^2 as the "squared sample correlation between the linear predictor
 # and the link-transformed response g(y)".
+# Additional sources include:
+# https://stats.stackexchange.com/questions/231365/calculating-a-pseudo-r2-value-when-deviance-is-negative/231383#231383
+# https://stats.stackexchange.com/questions/552281/calculating-different-pseudo-r2-for-a-betareg-model
 
-# linear predictor
+
+# Get linear predictor: Method 1
 lp <- m$coefficients$mean[1] + 
   as.matrix(datTrain[,list(iq, dyslexia_Ind, iq_dyslexia_Int= iq*dyslexia_Ind)]) %*% m$coefficients$mean[-1]
+lp <- setNames(as.numeric(lp), 1:length(lp))
+
+# Get linear predictor: Method 2
+lp2 <- predict(m, type = "link")
+
+# Equality?
+all.equal(lp, lp2) # TRUE
 
 # Pseudo R^2
 (pseudoRsquared <- (cor(log(m$y/(1-m$y)), lp)) ^ 2)
 
-pseudoRsquared == m$pseudo.r.squared
+pseudoRsquared == m$pseudo.r.squared # TURE
